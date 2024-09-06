b9a685dc9c4cf48ef9d948ee3bfa1d9e7bb81d14
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:33:49 +0200

    Add files via upload

---
+++ Fibonacci sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 0;
+int b = 3;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+for (int i = 2; i < 10; i++)
+{
+    int next = a + b;
+    a = b;
+    b = next;
+
+    Console.WriteLine(next);
+}
+
